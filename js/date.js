8b71996a46138469ed5c7487300bde126fb3b4b4
damian win <damianwin9demarzo@gmail.com>
2019-05-31 15:44:35 -0300

    Init commit

---
+++ js/date.js
@@ -0,0 +1,17 @@
+var d = new Date();
+var day;
+var dCount = d.getDay()
+
+if (dCount == 1) {
+    day = "monday"
+} else if (dCount >= 5) {
+    day = "working day"
+} else {
+    day = "feriado"
+}
+
+if (d.toString().match(/May 19/) == "May 19" || 
+d.toString().match(/May 20/) == "May 20") {
+    day = "holiday";
+} 
+console.log(day);
